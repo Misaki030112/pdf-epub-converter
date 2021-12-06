e3304fadca6c4bbfc7fb2fa6940d391939ad94aa
EmberCCC <1810888456@qq.com>
2021-12-06 23:41:00 +0800

    12.06 简单文本创建html且转换为epub文件

--- src/main/java/services/pdfToTxt.java
+++ src/main/java/services/pdfToTxt.java
@@ -1,4 +1,24 @@
 package services;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class pdfToTxt {
+
+
+    public void printPDFContext(String filePath,String fileName){
+        try{
+            File file = new File(filePath,fileName);
+            PDDocument load = PDDocument.load(file);
+            PDFTextStripper textStripper = new PDFTextStripper();
+            String txt = textStripper.getText(load);
+            System.out.println(txt);
+            load.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
