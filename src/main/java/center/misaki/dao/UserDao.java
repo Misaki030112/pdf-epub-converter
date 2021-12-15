f6e443d2e1e33c74e07bb6c297e3caaa1dbf4a85
EmberCCC <1810888456@qq.com>
2021-12-15 20:54:35 +0800

    12.15 完成单个pdf转换为Epub，保存路径在配置文件中

--- src/main/java/center/misaki/dao/UserDao.java
+++ src/main/java/center/misaki/dao/UserDao.java
@@ -2,7 +2,6 @@ package center.misaki.dao;
 
 import center.misaki.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface UserDao extends JpaRepository<User,Integer> {
